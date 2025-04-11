e4257d34ebf96ed2be6b1267f9d087693a98e43b
alireza <alirezafarmani619@gmail.com>
2025-04-11 23:34:29 +0330

    connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from "@nestjs/common";
-import { ProjectsModule } from './projects/projects.module';
+import { ProjectsModule } from "./projects/projects.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [ProjectsModule],
+  imports: [
+    ProjectsModule,
+    TypeOrmModule.forRoot({
+      // connect to db
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "",
+      database: "task_manager",
+      // add entities
+      entities: [__dirname + "/**/entities/*.entity{.ts,.js}"],
+      // synchronize for development mod
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
